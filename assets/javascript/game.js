d52a5a4737a6f089ae3223318bc4b90d681174f8
Melanie Corchado <melaniecorchado@Melanies-MacBook-Pro.local>
2017-05-23 18:35:08 -0400

    updated files

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	var random = ""
+	var wins = 0
+	var losses = 0
+	var aqua = 0
+	var pink = 0
+	var purple = 0
+	var leo = 0
+
+//this chooses the random number 
+	var random=Math.floor(Math.random())
+
+//shows in HTML
+	$('#randomNumber').text(random);
+	
+});
+
+//clicks for each crystal 
+
+ $("#aqua").click(function(){
+ 	});
+
+ $("#pink").click(function(){
+
+ });
+
+ $("#purple").click(function(){
+
+ });
+
+ $("#leo").click(function(){
+
+ });
